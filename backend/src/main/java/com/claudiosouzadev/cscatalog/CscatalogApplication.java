f07f14b02b2da9965c94a1e0198838d4664210c4
Claudiocia <claudiosouza.cia@gmail.com>
2021-03-20 20:31:28 -0300

    Project created

---
+++ backend/src/main/java/com/claudiosouzadev/cscatalog/CscatalogApplication.java
@@ -0,0 +1,13 @@
+package com.claudiosouzadev.cscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CscatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CscatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/claudiosouzadev/cscatalog/CscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.claudiosouzadev.cscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

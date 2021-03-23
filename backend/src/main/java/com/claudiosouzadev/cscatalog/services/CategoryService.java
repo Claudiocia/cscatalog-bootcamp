8b3c749846d23cd2a4d506929b146f423f0d93b4
Claudiocia <claudiosouza.cia@gmail.com>
2021-03-23 09:21:16 -0300

    DTO

--- backend/src/main/java/com/claudiosouzadev/cscatalog/services/CategoryService.java
+++ backend/src/main/java/com/claudiosouzadev/cscatalog/services/CategoryService.java
@@ -2,11 +2,13 @@ package com.claudiosouzadev.cscatalog.services;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.claudiosouzadev.cscatalog.dto.CategoryDTO;
 import com.claudiosouzadev.cscatalog.entities.Category;
 import com.claudiosouzadev.cscatalog.repositories.CategoryRepository;
 
@@ -17,9 +19,12 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
+	public List<CategoryDTO> findAll(){
+		 List<Category> list = repository.findAll();
 		 
-		return repository.findAll();
+		 return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		 
+	
 	}
 	
 }
